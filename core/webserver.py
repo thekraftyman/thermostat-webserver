ebdc5bab4a4469db3d49ad9995f459c367af348a
thekraftyman <thekraftyman@gmail.com>
2021-04-01 14:04:50 -0500

    first template finished

--- core/webserver.py
+++ core/webserver.py
@@ -1,9 +1,13 @@
-from flask import Flask
+from flask import Flask, render_template
+import datetime
+import os
 
 class Webserver:
 
     def __init__(self, port=80, host='0.0.0.0', debug=False):
-        self.app = Flask(__name__)
+        self.template_path = os.path.abspath('templates')
+        print(self.template_path)
+        self.app = Flask(__name__, template_folder=self.template_path)
         self.port = port
         self.host = host
         self.debug = debug
@@ -15,4 +19,10 @@ class Webserver:
     def add_routes(self):
         @self.app.route('/')
         def index():
-            return "<h1>Hello, world!</h1>"
+            now = datetime.datetime.now()
+            timeString = now.strftime("%Y-%m-%d %H:%M")
+            templateData = {
+                'title' : 'HELLO!',
+                'time': timeString
+            }
+            return render_template('index.html', **templateData)
