7bc2fd97a536600c8396657878cb09e094a5a14e
thekraftyman <thekraftyman@gmail.com>
2021-04-10 16:10:56 -0500

    update send

--- core/hvac_controller.py
+++ core/hvac_controller.py
@@ -2,6 +2,7 @@
 # By: Adam Kraft
 import json
 from os import system
+from time import sleep
 
 class HVAC_Controller:
 
@@ -12,18 +13,9 @@ class HVAC_Controller:
         self.mode = None
         self.temp = None
         self.fan  = None
-        self._modes = {
-            "OFF"    :  0,
-            "COOL"   :  1,
-            "DRY"    :  2,
-            "HEAT"   :  3,
-            "RESEND" : -1
-        }
-        self._fan_speeds = {
-            "AUTO" : 0,
-            "LOW"  : 1,
-            "HIGH" : 2
-        }
+        self.is_on = None
+        self._modes = ["OFF","COOL","DRY","HEAT","RESEND"]
+        self._fan_speeds = ["AUTO","LOW","HIGH"]
         self.load_config()
 
     def load_config(self):
@@ -44,8 +36,28 @@ class HVAC_Controller:
                 self._send(temp, mode, fan)
 
     def _send(self, temp, mode, fan):
-        system(self.build_command(temp, mode, fan))
+        ''' more logic here '''
+        command = f'irsend send_once {self.controller} '
 
-    def build_command(self, temp, mode, fan):
-        ''' builds the command that will be sent to the hvac unit '''
-        return f'irsend send_once {self.controller} {mode}-{fan}-{temp}{self.temp_mode}'
+        # make sure the mode will work
+        if mode != self.mode and self.mode:
+            system(command + f'turn-off')
+            self.is_on = False
+            sleep(2)
+
+        # first, turn the device on
+        if not self.is_on:
+            sytem(command + f'{mode}-on')
+            self.is_on = True
+            sleep(2)
+
+        # send the desired mode command
+        command += f'{mode}-{fan}-{temp}{self.temp_mode}'
+
+        # reassign the mode stuff
+        self.temp = temp if (self.temp != temp) else self.temp
+        self.mode = mode if (self.mode != mode) else self.mode
+        self.fan  = fan if (self.fan != fan) else self.fan
+
+        # send the command
+        system(command)
