bdf9b8f832ad33ad65e669eab91184dfcf417854
thekraftyman <thekraftyman@gmail.com>
2021-04-18 16:35:13 -0500

    save

---
+++ core/rgbled.py
@@ -0,0 +1,14 @@
+# rgbled.py
+# By: Adam Kraft
+
+from gpiozero import PWMLED as LED
+
+class RGBLED:
+
+    def __init__(self, red_pin, green_pin, blue_pin):
+        self.red_pin   = red_pin
+        self.green_pin = green_pin
+        self.blue_pin  = blue_pin
+        self.red_led   =
+        self.green_led =
+        self.blue_led  =
