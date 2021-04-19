b875f6dbe35c32778a1104f8a4616f4a1756c669
thekraftyman <thekraftyman@gmail.com>
2021-04-19 10:56:15 -0500

    trim rpi_setup

--- setup/rpi_setup.sh
+++ setup/rpi_setup.sh
@@ -1,9 +1,4 @@
 #!/bin/bash
 
 # install required packages
-apt install lirc
-apt 
-
-echo "Configuring one-wire..."
-echo "dtoverlay=w1-gpio" >> /boot/config.txt
-echo "Config added, a reboot is required to make the one-wire work"
+sudo apt install lirc
